d4afab7ab3f432e3b627b456d85b31e498f56ad9
matthiashaefeli <haefeli.matthias@gmail.com>
2018-04-02 17:49:02 -0500

    add crud courses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402195217) do
+ActiveRecord::Schema.define(version: 20180402210327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20180402195217) do
     t.string "name"
     t.string "lastname"
     t.integer "group"
+    t.boolean "admin", default: false
+    t.boolean "teacher", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
