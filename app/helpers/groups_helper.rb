dc3325f6437094478ae4689f88123649a121908c
matthiashaefeli <haefeli.matthias@gmail.com>
2018-04-03 15:29:19 -0500

    crud groups

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,27 @@
 module GroupsHelper
+
+    def teacher_array
+        teachers = Teacher.all
+        result_array = []
+        teachers.each do |t|
+            full_name = "#{t.user.name} #{t.user.lastname}"
+            result_array.push([full_name, t.id])
+        end
+        result_array
+    end
+
+    def course_array
+        courses = Course.all
+        result_array = []
+        courses.each do |c|
+            result_array.push([c.name, c.id])
+        end
+        result_array
+    end
+
+    def actual_teacher(teacher)
+        return ["#{teacher.user.name} #{teacher.user.lastname}", teacher.id]
+        
+    end
+
 end
