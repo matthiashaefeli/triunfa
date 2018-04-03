923a94b3bcfd5ed6c5642961e5eef41644ccf0a9
matthiashaefeli <haefeli.matthias@gmail.com>
2018-04-03 17:03:22 -0500

    add error if not exists group

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,37 +1,29 @@
 module WelcomeHelper
 
     def check_user(user)
-        admins = []
-        Admin.all.each do |a|
-            admins.push(a.user.email)
-        end
-        students = []
-        Student.all.each do |a|
-            students.push(a.user.email)
-        end
-        teachers = []
-        Teacher.all.each do |a|
-            teachers.push(a.user.email)
-        end
-
-        if students.include?(user.email)
-            return "student"
-        elsif teachers.include?(user.email)
-            return "teacher"
-        elsif admins.include?(user.email)
-            return "admin" 
-        else
-            return "new_user"
+        if user != nil
+            admins = []
+            Admin.all.each do |a|
+                admins.push(a.user.email)
+            end
+            students = []
+            Student.all.each do |a|
+                students.push(a.user.email)
+            end
+            teachers = []
+            Teacher.all.each do |a|
+                teachers.push(a.user.email)
+            end
+            
+            if students.include?(user.email)
+                return "student"
+            elsif teachers.include?(user.email)
+                return "teacher"
+            elsif admins.include?(user.email)
+                return "admin" 
+            else
+                return "new_user"
+            end
         end
     end
 end
-
-
-
-# admins.each do |admin|
-#     if admin.user == user
-#         return true
-#     else
-#         return false
-#     end
-# end
