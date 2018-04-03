c783a0297091c69edb8003c6ad46704bfc6a7909
matthiashaefeli <haefeli.matthias@gmail.com>
2018-04-03 16:07:50 -0500

    add check_user method

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,13 +1,37 @@
 module WelcomeHelper
 
-    def check_admin(user)
-        admins = Admin.all
-        admins.each do |admin|
-            if admin.user == user
-                return true
-            else
-                return false
-            end
+    def check_user(user)
+        admins = []
+        Admin.all.each do |a|
+            admins.push(a.user.email)
+        end
+        students = []
+        Student.all.each do |a|
+            students.push(a.user.email)
+        end
+        teachers = []
+        Teacher.all.each do |a|
+            teachers.push(a.user.email)
+        end
+
+        if students.include?(user.email)
+            return "student"
+        elsif teachers.include?(user.email)
+            return "teacher"
+        elsif admins.include?(user.email)
+            return "admin" 
+        else
+            return "new_user"
         end
     end
 end
+
+
+
+# admins.each do |admin|
+#     if admin.user == user
+#         return true
+#     else
+#         return false
+#     end
+# end
