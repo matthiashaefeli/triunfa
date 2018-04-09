9290e7d902932ec485507735b96add7d297f841d
matthiashaefeli <haefeli.matthias@gmail.com>
2018-04-09 17:30:47 -0500

    add limit on messages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 
     def index
         @rooms = Room.where(group: Group.where(activ: true))
-        @chats = Chat.order(created_at: :desc)
+        @chats = Chat.order(created_at: :desc).limit(10)
         @teacher_rooms = Room.where(group: Group.where(teacher: Teacher.find_by(user: current_user))).where(group: Group.where(activ: true)) 
     end
 
