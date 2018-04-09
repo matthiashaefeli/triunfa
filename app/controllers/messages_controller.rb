09103f8605ad874b608201dcada0cdd76842604f
matthiashaefeli <haefeli.matthias@gmail.com>
2018-04-09 10:50:33 -0500

    message action cable on different rooms

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,27 @@
 class MessagesController < ApplicationController
     def create
-        if params[:room]
             room = Room.find(params[:room])
-            message = Message.new(room: room, body: params[:message][:body], user: current_user)
-            message.save
-            redirect_to room_path(params[:room])
-
-        else
-            student = Student.find_by(user: current_user)
-            room = Room.find_by(group: student.group)
             message = Message.new(room: room, body: params[:message][:body], user: current_user, avatar: params[:message][:avatar])
-            message.save
-            redirect_to root_path
-
-        end
-        
-
+            if message.save
+                if message.avatar_file_name == nil
+                    imageUrl = ""
+                else
+                    imageUrl = message.avatar.url(:medium)
+                end
+                name = "#{message.user.name} #{message.user.lastname}"
+                ActionCable.server.broadcast 'message_channel',
+                                             content:  message.body,
+                                             name: name,
+                                             created: message.created_at,
+                                             image: imageUrl,
+                                             room: room.id
+            end
+            redirect_to room_path(params[:room])
     end
 end
+
+
+
+
+
+
