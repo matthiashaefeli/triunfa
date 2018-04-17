da6597aac70301a12cab8997a5ca5dc60b93af60
matthiashaefeli <haefeli.matthias@gmail.com>
2018-04-17 15:15:56 -0500

    add avatar to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [ :name, :lastname, :email, :password, :password_confirmation, :remember_me, :street, :city, :tel, :state, :cp]
+    added_attrs = [ :name, :lastname, :email, :password, :password_confirmation, :remember_me, :street, :city, :tel, :state, :cp, :avatar]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
