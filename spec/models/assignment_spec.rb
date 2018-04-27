dca6b5867e428d248a644904e1cd9fb2599aacf6
matthiashaefeli <haefeli.matthias@gmail.com>
2018-04-27 14:44:34 -0500

    finished model spec

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Assignment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:course){Course.new(name: "excel", start: "01-02-2018 10:10:10", end: "02-02-2018 10:10:10")}
+  let(:assignment){Assignment.new(name: "email", course: course)}
+
+  it "belongs to a course" do
+      expect(assignment.course).to eq course
+  end
+
+  it "has a name" do
+      expect(assignment.name).to eq "email"
+  end
 end
