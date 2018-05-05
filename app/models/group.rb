f37ef65248f6aaaa4a99ce048e34792c013a8e77
matthiashaefeli <haefeli.matthias@gmail.com>
2018-05-04 20:57:43 -0500

    validation model assignment group teacher user

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,4 +7,9 @@ class Group < ApplicationRecord
     belongs_to :teacher
     has_many :students
     has_many :tasks
+
+    validates :name, presence: true
+    validates :key, presence: true
+    validates :teacher, presence: true
+    validates :course, presence: true
 end
