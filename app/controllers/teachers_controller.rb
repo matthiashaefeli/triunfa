a7b9863d199df1367e44d921a61b70e7e3525777
matthiashaefeli <haefeli.matthias@gmail.com>
2018-05-07 15:40:05 -0500

    add service objects for who is

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,7 +1,8 @@
 class TeachersController < ApplicationController
-
+    include ServiceUser
+    
     def index
-        # who_is
+        who_is
         if params[:limit] != nil
             @talks_limit = params[:limit]
         else
@@ -63,19 +64,5 @@ class TeachersController < ApplicationController
         params.require(:teacher).permit(:name, :lastname, :email, :password)
     end
 
-    def who_is
-        if !current_user
-            redirect_to root_path
-        else
-            admins  = []
-            Admin.all.each do |a|
-                admins.push(a.user.email)
-            end
-            if admins.include?(current_user.email)
-                return true  
-            end
-            redirect_to root_path
-        end
-    end
 end
 
---
+++ app/services/service_user.rb
@@ -0,0 +1,18 @@
+module ServiceUser
+
+        def who_is
+            if !current_user
+                redirect_to root_path
+            else
+                admins  = []
+                Admin.all.each do |a|
+                    admins.push(a.user.email)
+                end
+                if admins.include?(current_user.email)
+                    return true  
+                end
+                redirect_to root_path
+            end
+        end
+
+end
