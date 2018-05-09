88e9abbe0fe423195e51a05e2dd933cb8f2e6151
matthiashaefeli <haefeli.matthias@gmail.com>
2018-05-09 15:30:45 -0500

    add docx files validates to paper model

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -9,7 +9,7 @@ class Paper < ApplicationRecord
                     },
                     :fog_directory => ENV["aws_library"]
 
-    validates_attachment_file_name :document, matches: [/\.xls?$/, /\.xlsx?$/, /\.csv?$/, /\.pdf?$/, /\.png?$/, /\.jpg?$/, /\.jpeg?$/]
+    validates_attachment_file_name :document, matches: [/\.xls?$/, /\.xlsx?$/, /\.csv?$/, /\.pdf?$/, /\.png?$/, /\.jpg?$/, /\.jpeg?$/, /\.docx?$/]
 
     process_in_background :document
 
