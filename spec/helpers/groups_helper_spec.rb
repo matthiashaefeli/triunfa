3d8f304401e6e15d8b0eb6e173dc7b22b0e7bede
matthiashaefeli <haefeli.matthias@gmail.com>
2018-05-22 16:17:32 -0500

    add more spec tests

--- spec/helpers/groups_helper_spec.rb
+++ spec/helpers/groups_helper_spec.rb
@@ -2,30 +2,32 @@ require 'rails_helper'
 
 RSpec.describe GroupsHelper, type: :helper do
 
-
-    let(:user){User.new(name: "pedro", lastname: "bosa", email: "bosa@test.com", password: "1234")}
-    let(:usertwo){User.new(name: "juan", lastname: "dora", email: "bosa@test.com", password: "1234")}
-    let(:userthree){User.new(name: "Sedano", lastname: "deda", email: "bosa@test.com", password: "1234")}
-    let(:teacher){Teacher.new(user: user)}
-    let(:teachertwo){Teacher.new(user: usertwo)}
-    let(:teacherthree){Teacher.new(user: userthree)}
+    setup do 
+    @user = User.create(name: "pedro", lastname: "bosa", email: "bosa1@test.com", password: "1234")
+    @usertwo = User.create(name: "juan", lastname: "dora", email: "bosa2@test.com", password: "1234")
+    @userthree = User.create(name: "Sedano", lastname: "deda", email: "bosa3@test.com", password: "1234")
+    @teacher = Teacher.create(user: @user)
+    teachertwo = Teacher.create(user: @usertwo, activ: true)
+    teacherthree = Teacher.create(user: @userthree)
+    course = Course.create(name: "excel", startdate: "10-01-2018", enddate: "10-02-2018")
+    end
 
 
     describe "teacher_array" do 
-        it "returns an array of all teachers fullname" do 
-            expect(teacher_array).to eq []
+        it "returns an array of all teachers fullname" do
+            expect(teacher_array.length).to eq 3
         end
     end
 
     describe "course_array" do 
         it "returns an array of all courses" do 
-            expect(course_array).to eq []
+            expect(course_array.length).to eq 1
         end
     end
 
     describe "actual_teacher" do 
         it "returns teacher data in an array" do 
-            expect(actual_teacher(teacher)).to eq ["pedro bosa", nil]
+            expect(actual_teacher(@teacher)).to eq ["pedro bosa", @teacher.id]
         end
     end
 
