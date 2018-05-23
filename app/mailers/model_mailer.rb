db5636addaa6953d1bbae71165b33960064404cd
matthiashaefeli <haefeli.matthias@gmail.com>
2018-05-23 14:08:57 -0500

    css and fonts

--- app/mailers/model_mailer.rb
+++ app/mailers/model_mailer.rb
@@ -9,7 +9,7 @@ include SendGrid
     from = Email.new(email: 'plataforma.superateytriunfa@gmail.com')
     to = Email.new(email: user.email)
     subject = 'Bienvenido'
-    content = Content.new(type: 'text/plain', value: "Bienvenido a Superate y Triunfa.\r\n\r\nTu grupo es: #{group.name}\r\n\r\nTu instructor es: #{group.teacher.user.name} #{group.teacher.user.lastname}\r\n\r\nhttp://www.superateytriunfaedu.org/")
+    content = Content.new(type: 'text/plain', value: "Bienvenido a Supérate y Triunfa.\r\n\r\nTu grupo es: #{group.name}\r\n\r\nTu instructor es: #{group.teacher.user.name} #{group.teacher.user.lastname}\r\n\r\nhttp://www.superateytriunfaedu.org/")
     mail = Mail.new(from, subject, to, content)
 
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
