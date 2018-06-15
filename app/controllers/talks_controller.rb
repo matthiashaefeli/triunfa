bc03caf4b4fd6e2a18035c3d59a380effa93e793
matthiashaefeli <haefeli.matthias@gmail.com>
2018-06-15 15:45:45 -0500

    add link to talks

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -19,7 +19,7 @@ class TalksController < ApplicationController
 
   private
   def talk_params
-    params.require(:talk).permit(:body, :avatar)
+    params.require(:talk).permit(:body, :avatar, :link)
   end
 
   def render_talk(talk)
