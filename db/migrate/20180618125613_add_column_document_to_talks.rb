7a26b2979edbd1ef55f50c0067b31d839193fa6e
matthiashaefeli <haefeli.matthias@gmail.com>
2018-06-18 08:01:19 -0500

    add file up and download to talk

---
+++ db/migrate/20180618125613_add_column_document_to_talks.rb
@@ -0,0 +1,8 @@
+class AddColumnDocumentToTalks < ActiveRecord::Migration[5.1]
+  def up
+    add_attachment :talks, :document
+  end
+  def down
+      remove_attachment :talks, :document
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180618124841) do
+ActiveRecord::Schema.define(version: 20180618125613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -266,6 +266,10 @@ ActiveRecord::Schema.define(version: 20180618124841) do
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string "link"
+    t.string "document_file_name"
+    t.string "document_content_type"
+    t.integer "document_file_size"
+    t.datetime "document_updated_at"
   end
 
   create_table "tasks", force: :cascade do |t|
