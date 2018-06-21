08fc3b5f063415ad3549050298deef53154993cb
matthiashaefeli <haefeli.matthias@gmail.com>
2018-06-21 10:44:44 -0500

    add tests for model

--- spec/models/grosery_spec.rb
+++ spec/models/grosery_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Grosery, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user){User.new(name: "teacher", lastname: "bosa", email: "bosa@test.com", password: "1234")}
+  let(:grocery){Grosery.new(name: "lunch")}
+  let(:grocerytwo){Grosery.new(name: "lunch", user: user)}
+
+  it "is not valid without a user" do
+    expect(grocery).to_not be_valid
+  end
+
+  it "is valid" do 
+    expect(grocerytwo).to be_valid
+  end
+
 end
