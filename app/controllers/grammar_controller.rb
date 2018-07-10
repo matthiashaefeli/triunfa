155e439b4c8f3ed09a4113dae76ade9df769d026
matthiashaefeli <haefeli.matthias@gmail.com>
2018-07-10 12:13:56 -0500

    change get text from tex area

--- app/controllers/grammar_controller.rb
+++ app/controllers/grammar_controller.rb
@@ -4,7 +4,7 @@ class GrammarController < ApplicationController
   require "net/http"
   def index
     if request.xhr?
-      text = params[:text].chop
+      text = params[:text]
 
       url = URI.parse("https://www.abbreviations.com/services/v2/grammar.php?uid=" +
                       ENV["grammar_user_id"] + "&tokenid=" + ENV["grammar_token"] + "&text=" + text)
