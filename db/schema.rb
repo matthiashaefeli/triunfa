8f8d7aa8a4bb4343744e13d1352da652d2ccc94a
matthiashaefeli <haefeli.matthias@gmail.com>
2018-07-30 10:56:25 -0500

    add column language to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180726144721) do
+ActiveRecord::Schema.define(version: 20180730155226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -374,6 +374,7 @@ ActiveRecord::Schema.define(version: 20180726144721) do
     t.date "birthdate"
     t.string "nationality"
     t.date "expired", default: "2018-01-01"
+    t.string "language", default: "spanish"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
