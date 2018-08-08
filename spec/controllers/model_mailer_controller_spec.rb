2c3104fcf6114759981ee0ed742e5dcf27f5135e
matthiashaefeli <haefeli.matthias@gmail.com>
2018-08-08 15:08:46 -0500

    add more tests

--- spec/controllers/model_mailer_controller_spec.rb
+++ spec/controllers/model_mailer_controller_spec.rb
@@ -4,4 +4,20 @@ require "rails_helper"
 
 RSpec.describe ModelMailerController, type: :controller do
 
+  let(:user) { User.create(name: "jon", lastname: "do", email: "j@j.com", password: "1243",
+  street: "street", city: "city", state: "state", tel: "123")} 
+
+  it "renders the :index view" do
+    create_admin(user)
+    sign_in(user)
+    get :index
+    expect(response).to render_template :index
+  end
+
+  it "redirect if user is not admin" do
+    sign_in(user)
+    get :index
+    expect(response.status).to eq (302)
+  end
+
 end
